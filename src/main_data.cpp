adc464ae733c15c74d8b2fb4055285f1b189e209
Rinnegatamante <rinnegatamante@gmail.com>
2016-04-18 01:29:05 +0200

    Bugfix for romFs support.
    Bugfix for romFs support.

--- src/main_data.cpp
+++ src/main_data.cpp
@@ -83,7 +83,7 @@ void Main_Data::Init() {
 	if (testfile != NULL){
 		Output::Debug("Detected a project on romFs filesystem...");
 		fclose(testfile);
-		project_path = "romfs:/";
+		project_path = "romfs:";
 		save_path = "sdmc:/";
 	}else 
 	#endif
